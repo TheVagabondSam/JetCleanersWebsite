db67ac8457ca8e3a00e92fa7f9dc7d9beb85a0a9
Thomaz Russian <Thomazrussian@gmail.com>
2024-10-23 16:44:42 -0300

    Update: Tailwind

--- src/app/components/ItemMenu.tsx
+++ src/app/components/ItemMenu.tsx
@@ -2,9 +2,9 @@ type Props = {
     name: string
     textColor?: string;
 }
-export function ItemMenu({name, textColor = 'text-white' } : Props) {
+export function ItemMenu({name, textColor = 'text-white'} : Props) {
     return (
-        <button className="hover:bg-light-blue flex items-center rounded gap-3">
+        <button className="hover:bg-light-blue flex items-center rounded">
             <span className={textColor}>
                 {name}
             </span>
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,34 +5,38 @@ import { ItemMenu} from "./ItemMenu";
 export function Header() {
     return (
         <>
-        <header className="flex w-full items-center justify-center h-10 bg-logo-blue">
-            <div className=" justify-center items-center">
-                    <ul className="flex gap-12 justify-center">
+        <header className="flex w-full items-center h-10 bg-logo-blue">
+            <div className=" w-full  max-w-[1246px] px-[15px] mx-auto font-bold">
+                <div>
+                    <div className="flex justify-center">
+                        <ul className="flex items-center gap-80">
                         <li>
                             <ItemMenu
                             name="Jetcleaners.queries@outlook.com"
                             />
                         </li>
-                        <li className="mr-80">
+                        <li className="">
                             <ItemMenu
                             name="+44 (0) 7535 315338"
                             />
                         </li>
                         <li>
                             <ItemMenu
-                            name="Get a queue now!"
+                            name="Get a quote!"
                             />
                         </li>
                         </ul>
                     </div>
+                </div>
+            </div>
         </header>
-        <header className="flex items-center relative w-full h-40 bg-white border-radius">
-                <div className="w-full flex h-40 ">
-                    <Image className="absolute translate-x-[50rem] translate-y-0 h-40 w-52"
+        <header className="flex items-center relative w-full h-40 font-bold bg-white border-radius">
+                <div className="w-full inline-flex items-center h-40">
+                    <Image className="absolute translate-x-[47.8rem] h-40 w-60"
                         src={Logo}
                         alt="Logo"
                     />
-                        <ul className="flex justify-center translate-x-40 items-center w-full gap-20">
+                        <ul className=" flex mx-auto justify-center items-center gap-20">
                             <li>
                                 <ItemMenu
                                 name="Home" textColor="text-logo-blue"
