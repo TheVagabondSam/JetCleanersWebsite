41c45eea3c6291b328ab60256bf32d2286405ff5
Thomaz Russian <Thomazrussian@gmail.com>
2024-11-06 17:33:02 -0300

    Feat/Cleanup

--- src/app/components/SectionBottom.tsx
+++ src/app/components/SectionBottom.tsx
@@ -1,32 +1,79 @@
 import Image from "next/image";
-import Insta from "/src/app/assets/insta.png"
-import Face from "/src/app/assets/face.png"
-import { Container } from "./Container"
+import Insta from "/src/app/assets/insta.svg"
+import Face from "/src/app/assets/face.svg"
+import Whatapp from "/src/app/assets/Whatapp.jpeg"
 import Logo from "/src/app/assets/whitelogo.png"
+import { Container } from "./Container"
 
 export function SectionBottom (){
     return(
-            <div className="w-full h-full items-center">
-                <div className="grid grid-cols-3 w-full h-80 px-20 mx-20 mt-2">
-                <div className="flex flex-col object-contain translate-x-1 w-[40%] place-self-center items-center h-32">
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="grid grid-cols-3 w-full h-80 px-20 mx-20 mt-2">
+                <div className="flex flex-row object-contain translate-x-10 w-[50%] place-self-center items-center h-80">
                     <Image className="max-w-[220px] flex-col"
                         src={Logo}
                         alt="Logo"
                     />
-                    <h1 className="text-center whitespace-pre-line">
-                        This is a placeholder and Sam is def a Weabo. Source? Trust me bro :)))
+                    <h1 className="text-center font-bold items-center translate-y-8 whitespace-pre-line">
+                        PlaceHolder
                     </h1>
                 </div>
-                <div className="flex flex-col h-32 object-contain items-center place-self-center">
-                    <Image className="place-self-center flex-col"
+                <div className="flex flex-col h-80 items-center object-contain px-4 gap-2 w-[45%] place-self-center">
+                    <div className="flex flex-row translate-y-28 gap-2">
+                    <Image className="w-12"
                     src={Insta}
                     alt="Instagram Logo"
                     />
-                    <h1>
-                        This is our Socials 
-                    </h1>
+                    <Image className="w-12"
+                    src={Face}
+                    alt="Facebook Logo"
+                    />
+                    <Image className="w-12"
+                    src={Whatapp}
+                    alt="Whatapp Logo"
+                    />
+                    </div>
+                    <span className="mt-1 font-bold translate-y-28 text-center items-center whitespace-pre-line">
+                        Our Socials
+                    </span>
                 </div>
+                <div className="grid grid-cols-2 items-center object-contain px-4 gap-2 w-full h-full place-self-center">
+                    <div className="items-center flex object-contain px-4 gap-2 w-full h-full place-self-center">
+                    <ul className="items-center w-full text-center">
+                        <li>
+                            <h1 className="font-bold text-2xl">
+                            Services
+                            </h1>
+                        </li>
+                        <li>
+                            Outdoor
+                        </li>
+                        <li>
+                            Indoor
+                        </li>
+                    </ul>
+                    </div>
+                    <div className="items-center flex object-contain px-4 gap-2 w-full h-full place-self-center">
+                        <ul className="items-center w-full text-center">
+                            <li>
+                                <h1 className="font-bold text-2xl">
+                                    Legal
+                                </h1>
+                            <li>
+                                Terms and conditions
+                            </li>
+                            <li>
+                                Privacy Policy
+                            </li>
+                            <li>
+                                Cookie Policy
+                            </li>
+                            </li>
+                        </ul>
+
+                    </div>
                 </div>
             </div>
+        </div>
     )
 }
--- src/app/components/SectionWhychoose.tsx
+++ src/app/components/SectionWhychoose.tsx
@@ -10,31 +10,31 @@ export function SectionWhychoose () {
             <h2 className="font-bold text-5xl  pt-10 w-[59.5%] grid text-black mx-auto ">
             Why Choose Jetcleaners?
             </h2>
-            <div className="grid w-[100%] place-items-center gap-2 grid-cols-3 pt-10 px-4">
-                <div className="flex flex-col items-center w-[54%]">
-                    <Image className="object-contain place-self-end h-20"
+            <div className="grid w-full place-items-center gap-8 grid-cols-3 pt-10 px-12">
+                <div className="flex flex-col items-center ml-auto">
+                    <Image className="object-contain h-20"
                     src={insured}
                     alt='insured'
                     />
-                    <span className="mt-2 text-right w-[97%]">
+                    <span className="text-center mt-2">
                         Placeholder
                     </span>
                 </div>
                 <div className="flex flex-col items-center">
-                    <Image className="object-contain justify-center h-20"
+                    <Image className="object-contain h-20"
                     src={staff}
                     alt='staff'
                     />
-                    <span className="mt-2 text-right w-[97%]">
+                    <span className="text-center mt-2">
                         Placeholder
                     </span>
                 </div>
-                <div className="flex flex-col items-center">
-                    <Image className="object-contain justify-self-start h-20"
+                <div className="flex flex-col items-center mr-auto">
+                    <Image className="h-20 object-contain"
                     src={fast}
                     alt='fast'
                     />
-                    <span className="mt-2 text-right w-[97%]">
+                    <span className="text-center mt-2">
                         Placeholder
                     </span>
                 </div>
