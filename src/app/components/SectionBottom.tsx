8bf6e53db1a69bf78a8ead34c652ea3b6e78328b
Thomaz Russian <Thomazrussian@gmail.com>
2024-11-07 19:45:31 -0300

    Fix SectionBottom

--- src/app/components/SectionBottom.tsx
+++ src/app/components/SectionBottom.tsx
@@ -8,19 +8,16 @@ import { ItemMenu } from "./ItemMenu";
 
 export function SectionBottom (){
     return(
-        <div className="w-full h-full flex items-center justify-center mt-24">
-            <div className="grid grid-cols-3 w-full h-80 px-20 mx-20 mt-2 gap-2">
-                <div className="flex justify-center w-full items-center h-full">
-                    <Image className="max-w-[260px] h-auto"
+        <div className="w-full h-full flex items-center justify-center mt-10">
+            <div className="grid grid-cols-4 gap-4 w-full h-[100%] justify-between px-20 mx-20 mt-4">
+                <div className="flex flex-col justify-end w-full items-center h-full gap-4">
+                    <Image className="max-w-[340px] h-auto"
                         src={Logo}
                         alt="Logo"
                     />
-                    <h1 className="text-center flex items-center h-full w-full whitespace-pre-line">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit repellat illo harum, quasi quas nulla eum vero distinctio neque officiis optio dicta aperiam quidem omnis. Soluta quasi dolores magni ratione.
-                    </h1>
                 </div>
-                <div className="flex flex-col h-[100%] justify-center items-center  px-4 gap-2 w-full place-self-center">
-                    <div className="flex flex-row w-full h-auto justify-center items-center gap-2">
+                <div className="flex flex-col h-[100%] justify-start items-center w-full px-4 gap-4 mt-16">
+                    <div className="flex w-full h-auto justify-center items-center gap-4">
                         <a href="https://www.instagram.com/jetcleanersltd/?utm_source=qr" target="_blank" rel="noopener noreferrer">
                             <Image className="w-12"
                             src={Insta}
@@ -46,18 +43,17 @@ export function SectionBottom (){
                             />
                         </a>
                     </div>
-                    <span className="mt-1 font-bold text-center items-center">
+                    <span className="mt-1 font-bold text-center text-logo-blue items-center">
                         Our Socials
                     </span>
                 </div>
-                <div className="grid grid-cols-2 items-start gap-2 w-full h-full place-self-center">
-                    <div className="items-center flex flex-col px-4 gap-2 w-full h-full">
+                <div className="grid grid-cols-2 items-start gap-4 w-full h-full place-self-center mt-20">
+                    <div className="items-center flex flex-col px-4 gap-4 w-full h-full">
                     <ul className="items-center w-full text-center">
-                        <li className="font-bold text-2xl mt-4">
-                            <ItemMenu
-                            name="Services"
-                            textColor="text-dark-blue"
-                            />
+                        <li className="font-bold flex text-2xl mt-4">
+                            <h1 className="py-2 px-4 items-center">
+                                Services
+                            </h1>
                         </li>
                         <li>
                             <ItemMenu
@@ -74,13 +70,13 @@ export function SectionBottom (){
                         </li>
                     </ul>
                     </div>
-                    <div className="items-center flex flex-col px-4 gap-2 w-full h-full">
-                        <ul className="items-center w-full text-center">
-                            <li className="font-bold text-2xl mt-4">
-                                <ItemMenu
-                                name="Legal"
-                                textColor="text-dark-blue"
-                            />
+                </div>
+                <div className="items-center flex flex-col px-4 gap-4 w-full h-full">
+                        <ul className="items-center w-full flex-col  text-center">
+                            <li className="font-bold flex text-2xl mt-4">
+                            <h1 className="py-2 px-4 items-center">
+                                Legal
+                            </h1>
                             </li>
                             <li> 
                                 <ItemMenu
@@ -102,7 +98,6 @@ export function SectionBottom (){
                             </li>
                         </ul>
                     </div>
-                </div>
             </div>
         </div>
     )
