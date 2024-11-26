7175cee0816ab5d03b313bda9ce36990e223ca70
Thomaz Russian <Thomazrussian@gmail.com>
2024-11-26 17:49:56 -0300

    Fix:Responsive

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,34 +13,31 @@ export function Header() {
 
     return (
         <>
-        <header className="flex w-full h-[112px] min-w-[375px] items-center min-h-[4rem] md:h-[4rem] bg-logo-blue">
-            <div className="w-full flex items-center text-center justify-center h-[100%]">
-                <ul className="md:w-[80%] text-nowrap flex flex-col sm:flex sm:flex-row items-center justify-center text-center w-[100%] text-sm sm:text-lg p-1">
-                        <li>
+        <div className='w-full h-full justify-between'>
+        <header className="flex flex-row w-full h-[112px] min-w-[375px] justify-between items-center min-h-[4rem] md:h-[4rem] bg-logo-blue">
+            <div className="w-full flex items-center text-center justify-center mx-auto h-[100%]">
+                <nav className="md:w-[90%] text-nowrap flex flex-col sm:flex sm:flex-row items-center justify-center text-center w-[100%] text-sm sm:text-lg">
                             <ItemMenu
                             name="Jetcleaners.queries@outlook.com"
                             href="/"
                             />
-                        </li>
-                        <li>
                             <ItemMenu
                             name="+44 (0) 7535 315338"
                             href="https://wa.me/4407535315338"
                             />
-                        </li>
-                        </ul>
-                <div className="sm:text-center text-start flex items-center h-[100%] max-w-5xl sm:pr-0 pr-6 sm:w-[100%]">
-                    <div className="flex items-center sm:px-0 px-1 max-w-[140px] sm:min-w-[140px] h-[100%]">
-                        <span className="md:w-full  h-auto w-[93%] bg-white text-start text-logo-blue whitespace-nowrap sm:text-lg font-bold rounded-lg">
+                        </nav>
+                <div className="sm:text-center text-start flex items-center justify-center h-[100%] sm:w-[100%]">
+                    <div className="flex items-center sm:px-0 px-1 max-w-[140px] ml-20 sm:min-w-[140px] h-[100%]">
+                        <nav className="md:w-full  h-auto w-[90%] bg-white text-start text-logo-blue whitespace-nowrap sm:text-lg font-bold rounded-lg">
                             <ItemMenu
                             name="Get a quote!"
                             textColor="text-logo-blue"
                             href="/ContactUsPage"
                             />
-                    </span>
-                    </div>
+                        </nav>
                     </div>
                 </div>
+            </div>
         </header>
 
         <button
@@ -63,9 +60,9 @@ export function Header() {
         </svg>
         </button>
 
-        <header className="flex items-center text-center w-[100%] h-[150px] font-bold bg-white">
-            <div className="h-[100%] w-full max-w-[670px] min-w-[200px] flex items-center mx-auto justify-center sm:justify-end">
-                <Image className="h-full object-contain bg-cover animate-slow-pulse"
+        <header className="text-center md:h-[150px] font-bold bg-white flex flex-row w-full min-w-[375px] items-center h-[190px]">
+            <div className="h-[100%] w-full flex items-center justify-center">
+                <Image className="h-full object-contain bg-cover min-w-[220px] animate-slow-pulse"
                     src= {Logo}
                     alt="Logo"
                     width={200}
@@ -74,11 +71,11 @@ export function Header() {
                 
             </div>
             <nav
-                className={`${isOpen ? 'flex' : 'hidden sm:flex'} flex flex-col sm:flex sm:flex-row sm:items-center items-start justify-center md:gap-6 sm:gap-1 h-[100%] sm:mx-1 w-full mb-2 px-2`}
+                className={`${isOpen ? 'flex' : 'hidden sm:flex'} flex flex-col text-center sm:flex sm:flex-row max-w-5xl  items-center sm:justify-start justify-center md:gap-4 lg:gap-6 xl:gap-10 sm:gap-1 h-[100%] w-full `}
             >
                         <ItemMenu
                             name="Home" textColor="text-logo-blue"
-                            href="/"
+                            href="/"    
                         />
                         <ItemMenu 
                             name="About us" textColor="text-logo-blue"
@@ -94,6 +91,7 @@ export function Header() {
                         />
                     </nav>
             </header>
+            </div>
         </>
         
     )
